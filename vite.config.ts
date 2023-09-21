4e19f58e9a8eee7068b994f0a9b488391d085667
Alexey <kalifull@mail.ru>
2023-09-22 00:57:15 +0300

    add vite for dev mode

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import svgr from 'vite-plugin-svgr';
+import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react(), svgr({ exportAsDefault: true })],
+    server: {
+      port: 3000,
+      open: true,
+    },
+    resolve: {
+      alias: [{ find: '@', replacement: '/src' }],
+    },
+    define: {
+      __API__: JSON.stringify(env.API_ENV),
+      __IS_DEV__: JSON.stringify(env.DEV_ENV),
+      __PROJECT__: JSON.stringify(env.PROJECT_ENV),
+    },
+  };
+});
